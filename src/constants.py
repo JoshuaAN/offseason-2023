7cb61b06d39fb09dde93453238ccccd3e2657825
JoshuaAN <joshn@team2363.org>
2023-05-04 18:03:42 -0400

    Add base code

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+class ElectricalConstants:
+    #  --------------- [fl, fr, rl, rr]s;
+    kDriveMotorPorts = [28, 26, 10, 12];
+    kSteerMotorPorts = [29, 27, 11, 13];
+    kAbsEncoderPorts = [43, 33, 45, 31];
+
+    kPigeonPort = 2
+
+class DriveConstants:
